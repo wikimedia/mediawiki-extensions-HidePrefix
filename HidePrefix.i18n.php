cd0a1c941930214b03b7f464ffb2b9963b2ac859
Raimond Spekking <raymond@users.mediawiki.org>
2012-03-05 21:20:53 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- HidePrefix.i18n.php
+++ HidePrefix.i18n.php
@@ -17,19 +17,85 @@
 	------------------------------------------------------------------------------------------------
 */
 
-$messages = array();
-
-/** English
- * @author Van de Bugger
+$messages = array();/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author Wizardist
  */
-$messages[ 'en' ] = array(
-	'hideprefix-desc' => 'Hides prefix (namespace name) in links and page titles',
+$messages['be-tarask'] = array(
+	'hideprefix-desc' => 'Хавае прэфікс (прастору назваў) у спасылках і загалоўках старонак',
 );
 
-/** Russian (Русский)
- * @author Van de Bugger
+/** German (Deutsch)
+ * @author Kghbln
  */
-$messages[ 'ru' ] = array(
+$messages['de'] = array(
+	'hideprefix-desc' => 'Ermöglicht das Ausblenden des Namensraumprefix in Links und bei Seitennamen',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'hideprefix-desc' => 'Chowa prefiks (mě mjenjowego ruma) w wótkazach a titelach bokow',
+);
+
+/** Spanish (Español)
+ * @author Armando-Martin
+ */
+$messages['es'] = array(
+	'hideprefix-desc' => 'Oculta el prefijo (o nombre del espacio de nombres) en los enlaces y en los títulos de las páginas',
+);
+
+/** French (Français)
+ * @author DavidL
+ */
+$messages['fr'] = array(
+	'hideprefix-desc' => 'Cacher le préfixe (espace de nom) dans les liens et les titres de pages',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'hideprefix-desc' => 'Agocha o prefixo (o nome do espazo de nomes) nas ligazóns e nos títulos das páxinas',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'hideprefix-desc' => 'Chowa prefiks (mjeno mjenoweho ruma) we wotkazach a titulach stronow',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'hideprefix-desc' => 'Celar le prefixo (le nomine del spatio de nomines) in ligamines e in titulos de pagina',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'hideprefix-desc' => 'Го скрива префиксот (именскиот простор) во врските и насловите на страниците',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'hideprefix-desc' => 'Verbergt het voorvoegsel (de naamruimtenaam) in verwijzingen en paginanamen',
+);
+
+/** Polish (Polski)
+ * @author BeginaFelicysym
+ */
+$messages['pl'] = array(
+	'hideprefix-desc' => 'Ukrywa przedrostek (nazwę przestrzeni nazw) w linkach i tytułach',
+);
+
+/** Russian (Русский) */
+$messages['ru'] = array(
 	'hideprefix-desc' => 'Убирает префикс из ссылок и заголовков страниц',
 );
 
